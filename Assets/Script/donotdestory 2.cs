544083af54f69a0dcc2ce5e83ea859268ab437e5
meohyun26 <84959523+meohyun@users.noreply.github.com>
2023-06-12 23:11:26 +0900

    2023.06.12 23:11

---
+++ Assets/Script/donotdestory 2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class donotdestory : MonoBehaviour
+{
+    public static donotdestory s_Instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (s_Instance)
+        {
+            DestroyImmediate(this.gameObject);
+            return;
+        }
+        s_Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+}
